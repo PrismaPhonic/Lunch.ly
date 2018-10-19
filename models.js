2f0b27f04bd22467def6890a4cf77d21bd23b2ac
Peter Farr <Peter@PrismaPhonic.com>
2018-10-19 16:17:21 -0700

    fixed all routes and models

--- models.js
+++ models.js
@@ -10,7 +10,7 @@ db.connect();
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
     this.id = id;
     this.customerId = customerId;
     this.numGuests = numGuests;
@@ -43,6 +43,25 @@ class Reservation {
     return this._notes;
   }
 
+  /** numGuests setter/getter pattern */
+
+  set numGuests(val) {
+    if (val < 1) {
+      let err = new Error('Must have at least 1 in your party to make a reservation');
+      err.status = 422;
+      throw err;
+    } else if (isNaN(val)) {
+      let err = new Error('Number of guests must be a valid number');
+      err.status = 400;
+      throw err;
+    }
+    this._numGuests = val;
+  }
+
+  get numGuests() {
+    return this._numGuests;
+  }
+
   /** methods for setting/getting customer ID: can only set once. */
 
   set customerId(val) {
@@ -59,14 +78,14 @@ class Reservation {
 
   static async getReservationsForCustomer(customerId) {
     const results = await db.query(
-          `SELECT id, 
+      `SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt", 
            notes AS "notes"
          FROM reservations 
          WHERE customer_id = $1`,
-        [customerId]
+      [customerId]
     );
 
     return results.rows.map(row => new Reservation(row));
@@ -75,18 +94,34 @@ class Reservation {
   /** find a reservation by id. */
 
   static async get(id) {
-    const result = await db.query(
-          `SELECT id, 
+    const results = await db.query(
+      `SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt",
            notes
          FROM reservations 
-         WHERE id = $1`,
-        [id]
+         WHERE id = $1;`,
+      [id]
     );
+    return new Reservation(results.rows[0]);
+  }
 
-    return new Reservation(results.row[0]);
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customerId, this.numGuests, this.startAt, this.notes]);
+      console.log("result", result.rows[0].id);
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE reservations SET customer_id=$1, num_guests=$2, start_at=$3, notes=$4
+             WHERE id=$5`,
+        [this.customer_id, this.numGuests, this.startAt, this.notes, this.id]);
+    }
   }
 }
 
@@ -94,9 +129,10 @@ class Reservation {
 /** Customer of the restaurant. */
 
 class Customer {
-  constructor({id, firstName, lastName, phone, notes}) {
+  constructor({ id, firstName, middleName, lastName, phone, notes }) {
     this.id = id;
     this.firstName = firstName;
+    this.middleName = middleName;
     this.lastName = lastName;
     this.phone = phone;
     this.notes = notes;
@@ -122,12 +158,55 @@ class Customer {
     return this._phone;
   }
 
+
+  /** methods for getting/setting first and last name
+   *  so they are stored lowercase in our db
+   */
+
+  set firstName(str) {
+    this._firstName = str.toLowerCase();
+  }
+
+  get firstName() {
+    return this._firstName.replace(/\w/, c => c.toUpperCase());
+  }
+
+  set middleName(str) {
+    if (str) {
+      this._middleName = str.toLowerCase();
+    }
+  }
+
+  get middleName() {
+    if (this._middleName) {
+      return this._middleName.replace(/\w/, c => c.toUpperCase());
+    }
+  }
+
+  set lastName(str) {
+    this._lastName = str.toLowerCase();
+  }
+
+  get lastName() {
+    return this._lastName.replace(/\w/, c => c.toUpperCase());
+  }
+
+  /** methods for getting/setting full name. */
+
+  get fullName() {
+    if (this.middleName) {
+      return `${this.firstName} ${this.middleName} ${this.lastName}`;
+    }
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   /** find all customers. */
 
   static async all() {
     const results = await db.query(
-          `SELECT id, 
-         first_name AS "firstName",  
+      `SELECT id, 
+         first_name AS "firstName",
+         middle_name AS "middleName", 
          last_name AS "lastName", 
          phone, 
          notes
@@ -137,17 +216,60 @@ class Customer {
     return results.rows.map(c => new Customer(c));
   }
 
+  /** filter all customers by a search */
+
+  static async customerSearch(search) {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",
+         middle_name AS "middleName",
+         last_name AS "lastName"
+       FROM customers
+       WHERE first_name ILIKE $1 OR last_name ILIKE $1
+       ORDER BY last_name, first_name`,
+      [`${search}%`]
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+  /** Return top 10 customers by reservation count */
+
+
+  static async topTenReservationHolders() {
+    const results = await db.query(
+      `SELECT customers.id, 
+           customers.first_name AS "firstName",
+           customers.middle_name AS "middleName", 
+           customers.last_name AS "lastName",
+           count(*)
+         FROM customers
+         JOIN reservations ON customers.id = reservations.customer_id
+         GROUP BY customers.id
+         ORDER BY count desc
+         LIMIT 10;
+      `
+    );
+
+    results.rows.forEach((r, i) => {
+      delete r.count;
+      results.rows[i] = new Customer(r);
+    })
+
+    return results.rows;
+  }
+
   /** get a customer by ID. */
 
   static async get(id) {
     const results = await db.query(
-          `SELECT id, 
-         first_name AS "firstName",  
+      `SELECT id, 
+         first_name AS "firstName",
+         middle_name AS "middleName",  
          last_name AS "lastName", 
          phone, 
          notes 
         FROM customers WHERE id = $1`,
-        [id]
+      [id]
     );
     return new Customer(results.rows[0]);
   }
@@ -163,20 +285,20 @@ class Customer {
   async save() {
     if (this.id === undefined) {
       const result = await db.query(
-            `INSERT INTO customers (first_name, last_name, phone, notes)
-             VALUES ($1, $2, $3, $4)
+        `INSERT INTO customers (first_name, middle_name, last_name, phone, notes)
+             VALUES ($1, $2, $3, $4, $5)
              RETURNING id`,
-          [this.firstName, this.lastName, this.phone, this.notes]);
+        [this.firstName, this.middleName, this.lastName, this.phone, this.notes]);
       console.log("result", result.rows[0].id);
       this.id = result.rows[0].id;
     } else {
       await db.query(
-            `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4)
-             WHERE id=$5`,
-          [this.firstName, this.lastName, this.phone, this.notes, this.id]);
+        `UPDATE customers SET first_name=$1, middle_name=$2, last_name=$3, phone=$4, notes=$5
+             WHERE id=$6`,
+        [this.firstName, this.middleName, this.lastName, this.phone, this.notes, this.id]);
     }
   }
 }
 
 
-module.exports = {Customer, Reservation};
+module.exports = { Customer, Reservation };
--- routes.js
+++ routes.js
@@ -10,9 +10,23 @@ router = express.Router();
 
 router.get("/", async (req, res) => {
   const customers = await Customer.all();
+  console.log(customers);
   res.render("customer_list.html", { customers })
 });
 
+router.post("/", async (req, res) => {
+  let search = req.body.search;
+  const customers = await Customer.customerSearch(search);
+  res.render("customer_list.html", { customers })
+});
+
+/** Top 10 customers route */
+
+router.get("/top-ten", async (req, res) => {
+  const customers = await Customer.topTenReservationHolders();
+  console.log(customers);
+  res.render("topten_customers.html", { customers })
+});
 
 /** Form to add a new customer. */
 
@@ -27,10 +41,11 @@ router.post("/add/", async (req, res) => {
   try {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
+    const middleName = req.body.middleName || undefined;
     const phone = req.body.phone;
     const notes = req.body.notes;
 
-    const customer = new Customer({ firstName, lastName, phone, notes });
+    const customer = new Customer({ firstName, middleName, lastName, phone, notes });
     await customer.save()
 
     return res.redirect(`/${customer.id}/`);
@@ -68,15 +83,16 @@ router.get("/:customerId/edit/", async (req, res) => {
 /** Handle editing a customer. */
 
 router.post("/:customerId/edit/", async (req, res) => {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const phone = req.body.phone;
-  const notes = req.body.notes;
-
   try {
-    const customer = new Customer({ firstName, lastName, phone, notes });
-    await customer.save()
+    const customer = await Customer.get(req.params.customerId);
 
+    customer.firstName = req.body.firstName;
+    customer.lastName = req.body.lastName;
+    customer._phone = req.body.phone;
+    customer._notes = req.body.notes;
+    console.log(customer);
+
+    await customer.save();
     return res.redirect(`/${customer.id}/`);
   } catch (e) {
     return res.status(500).send(`Can't edit customer: ${e}`);
@@ -98,7 +114,38 @@ router.post("/:customerId/add-reservation/", async (req, res) => {
 
     return res.redirect(`/${customerId}/`);
   } catch (e) {
-    return res.status(500).send(`Can't create reservation: ${e}`);
+    res.status(e.status || 500);
+    return res.json({ error: `${e}` });
+  }
+});
+
+
+/** Show form to edit a reservation. */
+
+router.get("/reservations/:reservationId/edit/", async (req, res) => {
+  try {
+    const reservation = await Reservation.get(req.params.reservationId);
+    res.render("reservation_edit_form.html", { reservation });
+  } catch (e) {
+    return res.status(500).send(`Can't get reservation: ${e}`);
+  }
+});
+
+
+/** Handle editing a reservation. */
+
+router.post("/reservations/:reservationId/edit/", async (req, res) => {
+  try {
+    const reservation = await Reservation.get(req.params.reservationId);
+
+    reservation.startAt = req.body.startAt;
+    reservation.numGuests = req.body.numGuests;
+    reservation.notes = req.body.notes;
+
+    await reservation.save();
+    return res.redirect(`reservations/${reservation.id}/`);
+  } catch (e) {
+    return res.status(500).send(`Can't edit customer: ${e}`);
   }
 });
 
